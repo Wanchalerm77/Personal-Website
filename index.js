b1dbe00f6570fea4f9aa1cde2cd07504484a481c
Wanchalerm Honghom <wanhonghom@online.de>
2018-03-23 10:50:35 +0100

    d

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hbs = require("express-handlebars");
 
 
 let app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname ,"dist")));
 console.log(path.join(__dirname, "dist"));
