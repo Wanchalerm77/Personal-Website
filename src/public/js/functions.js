20cd606f28d3389959a4eade902d898d3b7e22eb
Wanchalerm Honghom <wanhonghom@online.de>
2018-03-24 08:20:47 +0100

    small adjustments

--- src/public/js/functions.js
+++ src/public/js/functions.js
@@ -42,5 +42,50 @@ let navbarAnimations = () => {
   });
 };
 
-
 navbarAnimations();
+
+
+//hobbies gallery
+
+var id;
+
+let slideShow = () => {
+  $(".hobby").hover(function() {
+    let img = $(this).find("img");
+    id = setInterval(() => {
+      for (let i = 0, k = 1; i < img.length; i++) {
+        $(img[i]).animate({
+              "opacity": 1
+            },
+            1000 * k)
+          .animate({
+            "opacity": 0
+          }, 1000);
+        k += 1.5;
+      }
+    }, 2550)
+  }, function() {
+    clearInterval(id);
+    let images = $(this).find("img");
+    for (let img of images) {
+      $(img).finish();
+    }
+  });
+};
+
+let onMouseEnter = () => {
+    $(".hobby").mouseenter(function() {
+      let images = $(this).find("img");
+      for (let i = 0, k = 1; i < images.length; i++) {
+        $(images[i]).animate({
+          "opacity": 1
+        }, 1000 * k).animate({
+          "opacity": 0
+        }, 1000);
+        k += 1.5;
+      }
+    });
+
+};
+onMouseEnter();
+slideShow();
